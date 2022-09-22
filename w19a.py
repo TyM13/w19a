13df6e4b328c54298c46aa27df84e96ea7aa4f3f
Your Name <you@example.com>
2022-09-21 19:14:50 -0600

    w19a fizzBuzz

---
+++ w19a.py
@@ -0,0 +1,13 @@
+def fizzbuzz(number):
+    for number in random_numbers:
+        if((number % 3 == 0)and number % 5 == 0):
+            print(number,'fizzbuzz')
+        elif(number % 5 == 0):
+            print(number,'buzz')
+        elif(number % 3 == 0):
+            print(number,'fizz')
+        else:
+            print(number,'something went wrong')
+
+random_numbers = [3,5,7,9,30,300,25,54,60,22,1535,12464567,123,122,150]
+fizzbuzz(random_numbers)
