743e2a17ded9dcd44832fd3f46c37b460cf01beb
Your Name <you@example.com>
2022-09-22 16:35:35 -0600

    put loop outside of func

--- w19a.py
+++ w19a.py
@@ -1,13 +1,14 @@
-def fizzbuzz(number):
-    for number in random_numbers:
-        if((number % 3 == 0)and number % 5 == 0):
-            print(number,'fizzbuzz')
-        elif(number % 5 == 0):
-            print(number,'buzz')
-        elif(number % 3 == 0):
-            print(number,'fizz')
-        else:
-            print(number,'something went wrong')
+def fizzbuzz(number):      
+    if((number % 3 == 0)and number % 5 == 0):
+        print(number,'fizzbuzz')
+    elif(number % 5 == 0):
+        print(number,'buzz')
+    elif(number % 3 == 0):
+        print(number,'fizz')
+    else:
+        print(number,'something went wrong')    
 
 random_numbers = [3,5,7,9,30,300,25,54,60,22,1535,12464567,123,122,150]
-fizzbuzz(random_numbers)
+
+for number in random_numbers:
+    fizzbuzz(number)
